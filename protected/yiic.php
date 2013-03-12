eab23d0c8ed4b0084ea20336cb6d1c7dc12db11d
cegrif01 <cegrif01@gmail.com>
2013-03-12 17:34:50 -0400

    first commit

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../WebFramework_mine/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
